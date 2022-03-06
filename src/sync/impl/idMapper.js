da620dd75f8ece3073e0db7734115238274bb0a9
David Huang <david@toodledo.net>
2022-03-06 11:55:46 -0500

    error message for missing server relation

--- src/sync/impl/idMapper.js
+++ src/sync/impl/idMapper.js
@@ -59,10 +59,10 @@ export function convertRelatedRemoteToLocalIds(
     // if remoteId is there (e.g. not 0 or "0") and can't find the localId, then log an error
     if (remoteId * 1 && !localId) {
       console.log(
-        `[Sync] Server wants client to map a record ${table}#${remoteId} that doesn't exist locally.`,
+        `[Sync] Server wants client to map a record ${table}#${remoteId} that doesn't exist locally. Will just ignore the mapping on the client, but this is probably a bug.`,
       )
       logError(
-        `[Sync] Server wants client to map a record ${table}#${remoteId} that doesn't exist locally.`,
+        `[Sync] Server wants client to map a record ${table}#${remoteId} that doesn't exist locally. Will just ignore the mapping on the client, but this is probably a bug.`,
       )
       return
     }
